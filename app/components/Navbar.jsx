cb4c1b7fe0a0a1cb277b870b2724e989099c2fdb
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-08-08 23:32:56 +0100

    <Image /> component (logo)

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+import Logo from "./dojo-logo.png";
 import Link from "next/link";
 
 export default function Navbar() {
   return (
     <nav>
+      <Image
+        src={Logo}
+        alt="HelpDesk logo"
+        width={70}
+        quality={100}
+        placeholder="blur"
+      />
       <h1>NetNinja Helpdesk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
