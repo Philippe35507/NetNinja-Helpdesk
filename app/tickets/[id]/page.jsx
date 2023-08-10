9d82aa5212a58b8e575eba6af4b56d3f4d5e43e2
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-08-10 18:34:59 +0100

    Fetching a Ticket

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,4 +1,12 @@
-export default function TicketDetails({ params }) {
-  const id = params.id;
-  return <div>{id}</div>;
+async function getTicket(id) {
+  const res = await fetch("http://localhost:4000/tickets" + id, {
+    next: { revalidate: 60 },
+  });
+
+  return res.json();
+}
+
+export default async function TicketDetails({ params }) {
+  const ticket = await getTicket(params.id);
+  return <div></div>;
 }
