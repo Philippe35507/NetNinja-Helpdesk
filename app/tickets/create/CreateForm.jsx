def86a4ece6bc64dd2063511cd7de1348aabd425
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-08-23 23:02:43 +0100

    Creating the Form itself

--- app/tickets/create/CreateForm.jsx
+++ app/tickets/create/CreateForm.jsx
@@ -11,5 +11,35 @@ export default function CreateForm() {
   const [priority, setPriority] = useState("low");
   const [isLoading, setIsLoading] = useState(false);
 
-  return <div>CreateForm</div>;
+  return (
+    <form className="w-1/2">
+      <label>
+        <span>Title:</span>
+        <input
+          required
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        ></input>
+      </label>
+      <label>
+        <span>Body:</span>
+        <textarea
+          required
+          type="text"
+          onChange={(e) => setBody(e.target.value)}
+          value={body}
+        ></textarea>
+        <select onChange={(e) => setPriority(e.target.value)} value={priority}>
+          <option value="low">Low Priority</option>
+          <option value="medium">Medium Priority</option>
+          <option value="high">High Priority</option>
+        </select>
+      </label>
+      <button className="btn-primary" disabled={isLoading}>
+        {isLoading && <span>Adding ...</span>}
+        {!isLoading && <span>Add Ticket</span>}
+      </button>
+    </form>
+  );
 }
