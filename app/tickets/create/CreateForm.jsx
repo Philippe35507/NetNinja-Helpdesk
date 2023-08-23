b36bcb14de8a513e0cb0fb73c8fc33c6b0e69c19
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-08-23 22:52:43 +0100

    Form: creating client + server components
    Nesting the client form component (CreateForm) inside of a server component (page)
    UseRouter: to redirect the user to the ticket's page

---
+++ app/tickets/create/CreateForm.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function CreateForm() {
+  const router = useRouter();
+
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [priority, setPriority] = useState("low");
+  const [isLoading, setIsLoading] = useState(false);
+
+  return <div>CreateForm</div>;
+}
