026d18697ec0d906cd55421d3047f1976b452e8b
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-08-25 12:19:37 +0100

    Update loading.jsx import

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import TicketList from "./TicketList";
-import Loading from "../Loading";
+import Loading from "../(dashboard)/loading";
 
 export default function Tickets() {
   return (
