16d1e748666f3d584117acb6c5d842382eb7a8f3
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-09-03 17:16:30 +0100

    Logging users out
    Button needs to be in a client component (click event and redirect)

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Logo from "./dojo-logo.png";
 import Link from "next/link";
+import LoggoutButton from "./LoggoutButton";
 
 export default function Navbar({ user }) {
   return (
@@ -14,8 +15,11 @@ export default function Navbar({ user }) {
       />
       <h1>NetNinja Helpdesk</h1>
       <Link href="/">Dashboard</Link>
-      <Link href="/tickets">Tickets</Link>
+      <Link href="/tickets" className="mr-auto">
+        Tickets
+      </Link>
       {user && <span>Hello, {user.email}</span>}
+      <LoggoutButton />
     </nav>
   );
 }
