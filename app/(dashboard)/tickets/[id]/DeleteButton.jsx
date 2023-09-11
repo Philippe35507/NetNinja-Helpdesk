0c884cb481abca03a5be5b447523daae10cc74c2
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-09-11 20:19:00 +0100

    Creating Delete Button

---
+++ app/(dashboard)/tickets/[id]/DeleteButton.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import { TiDelete } from "react-icons/ti";
+
+export default function DeleteButton() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = async () => {
+    setIsLoading(true);
+  };
+
+  return (
+    <button className="btn-primary" onClick={handleClick} disabled={isLoading}>
+      {isLoading && (
+        <>
+          <TiDelete />
+          Deleting ...
+        </>
+      )}
+      {!isLoading && (
+        <>
+          <TiDelete />
+          Delete Ticket
+        </>
+      )}
+    </button>
+  );
+}
