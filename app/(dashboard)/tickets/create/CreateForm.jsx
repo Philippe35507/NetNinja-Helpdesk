99590fde79a4c236835809f1dec1e327534fb9a0
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-09-15 11:55:20 +0100

    Handling a POST Request in a Server Action

--- app/(dashboard)/tickets/create/CreateForm.jsx
+++ app/(dashboard)/tickets/create/CreateForm.jsx
@@ -1,66 +1,24 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { addTicket } from "../actions";
 
 export default function CreateForm() {
-  const router = useRouter();
-
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-  const [priority, setPriority] = useState("low");
   const [isLoading, setIsLoading] = useState(false);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
-
-    const newTicket = {
-      title,
-      body,
-      priority,
-    };
-
-    const res = await fetch("http://localhost:3000/api/tickets", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newTicket),
-    });
-
-    const json = await res.json();
-    if (json.error) {
-      console.log(error.message);
-    }
-    if (json.data) {
-      router.refresh();
-      router.push("/tickets");
-    }
-  };
 
   return (
     <form action={addTicket} className="w-1/2">
       <label>
         <span>Title:</span>
-        <input
-          required
-          type="text"
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}
-        ></input>
+        <input name="title" required type="text"></input>
       </label>
       <label>
         <span>Body:</span>
-        <textarea
-          required
-          type="text"
-          onChange={(e) => setBody(e.target.value)}
-          value={body}
-        ></textarea>
+        <textarea name="body" required type="text"></textarea>
       </label>
       <label>
         <span>Priority:</span>
-        <select onChange={(e) => setPriority(e.target.value)} value={priority}>
+        <select name="priority">
           <option value="low">Low Priority</option>
           <option value="medium">Medium Priority</option>
           <option value="high">High Priority</option>
